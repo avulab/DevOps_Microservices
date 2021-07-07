135dce86bac331363efb8b4cad1610d248e1b386
EC2 Default User <ec2-user@ip-172-31-37-223.us-west-2.compute.internal>
2021-07-07 00:59:14 +0000

    First Changes

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -3,10 +3,14 @@
 ## Complete the following steps to get Docker running locally
 
 # Step 1:
-# Build image and add a descriptive tag
+# Build image and add a descriptive tag 
+docker build --tag project4:1.1 .
 
 # Step 2: 
 # List docker images
+docker images
 
 # Step 3: 
 # Run flask app
+#docker run -d -p  8000:80 project4:1.0 --name project4
+docker run -p  8000:80 project4:1.1
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=bavula/project4:1.1
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login -u bavula -p Arsenal8
+docker tag project4:1.1 $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push bavula/project4:1.1
